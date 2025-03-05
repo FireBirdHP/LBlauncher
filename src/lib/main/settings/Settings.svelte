3a9e72c43900a99ec64365b24f184cb21900fe76
FireBirdHP <sashaban2011@outlook.com>
2025-03-05 18:07:28 +0500

    qwe

---
+++ src/lib/main/settings/Settings.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    import GeneralSettings from "./GeneralSettings.svelte";
+    import PremiumSettings from "./PremiumSettings.svelte";
+    import SettingsContainer from "../../settings/SettingsContainer.svelte";
+    import Tabs from "../../settings/tab/Tabs.svelte";
+
+    export let options;
+    let activeSettingsTab = "General";
+
+    const dispatch = createEventDispatcher();
+</script>
+
+<SettingsContainer
+        title="Settings"
+        on:hideSettings={() => dispatch('hide')}
+>
+    <Tabs
+            tabs={["General", "Premium"]}
+            bind:activeTab={activeSettingsTab}
+            slot="tabs"
+    />
+
+    {#if activeSettingsTab === "General"}
+        <GeneralSettings
+                bind:options
+        />
+    {:else if activeSettingsTab === "Premium"}
+        <PremiumSettings
+                bind:options
+        />
+    {/if}
+</SettingsContainer>
